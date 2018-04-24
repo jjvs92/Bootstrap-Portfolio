dbb62da932118df64d7d673d4b2f5c8a61be82df
jjvs92 <julian_jvs92@hotmail.com>
2018-04-23 22:59:42 -0500

    Added some javascript to and modifed portfolio page

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    console.log("making sure it works")
+
+    $(".container").on("click", "#ccImage", function(){
+        window.open("https://jjvs92.github.io/unit-4-game/");
+    } )
+
+    $(".container").on("click", "#psychicGameImage", function(){
+        window.open("https://jjvs92.github.io/Psychic-Game/");
+    })
+})
